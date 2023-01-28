f2392db0354c2aa11e986027ab3287212bcdeef5
DamirSD673 <damir.sitdikov@tu-ilmenau.de>
2023-01-28 12:08:59 +0100

    two channel adc2dma

--- rri_control_1.0/hdl/rri_control_v1_0_S_ADC2DMA_AXIS.vhd
+++ rri_control_1.0/hdl/rri_control_v1_0_S_ADC2DMA_AXIS.vhd
@@ -17,6 +17,7 @@ entity rri_control_v1_0_S_ADC2DMA_AXIS is
 		-- Users to add ports here
 		dma_output_enable : in std_logic;		
 		dma_last_value : in std_logic;
+		dma_channel : in std_logic_vector(1 downto 0);
 
 		-- AXI4Stream Clock
 		AXIS_ACLK	: in std_logic;
@@ -57,7 +58,9 @@ architecture arch_imp of rri_control_v1_0_S_ADC2DMA_AXIS is
 	-- State variable
 	signal  mst_exec_state : state := IDLE;  
 	-- FIFO implementation signals
-	signal  data_in_reg: std_logic_vector(C_M_AXIS_TDATA_WIDTH-1 downto 0);
+	signal data_in_reg : std_logic_vector(C_M_AXIS_TDATA_WIDTH-1 downto 0);
+	signal choose_enable : std_logic := '1';
+	signal dma_channel_active: std_logic_vector(1 downto 0);
 	
 	signal data_counter : unsigned(1 downto 0);
 	signal data_counter_last : unsigned(1 downto 0);
@@ -95,82 +98,133 @@ begin
 	
 	process (AXIS_ACLK)
 	begin
-	  if rising_edge(AXIS_ACLK) then 
-	    if S_AXIS_ARESETN = '0' then
-	      data_counter <= b"00";
-	      dma_data_last_delay <= '0';
+		if rising_edge(AXIS_ACLK) then 
+			if S_AXIS_ARESETN = '0' then
+	      	data_counter <= b"00";
+	      	dma_data_last_delay <= '0';
 	      
-	      data_counter_last <= b"00";
-	      data_in_reg <= (others => '0');
-	      dma_enable_delay <= '0';
-	      write_enable <= '0';
-	      dma_running_int <= '0';
+	      	data_counter_last <= b"00";
+	      	data_in_reg <= (others => '0');
+	      	dma_enable_delay <= '0';
+	      	write_enable <= '0';
+	      	dma_running_int <= '0';
 	    else
-	       dma_enable_delay <= dma_output_enable;
-	       dma_data_last_delay <= dma_last_value;
-	       if (dma_output_enable = '1') then
-	           case data_counter is
-	               when "00" =>
-	                   data_in_reg(15 downto 0) <= S_AXIS_TDATA(15 downto 0);
-	               when "01" =>
-	                   data_in_reg(31 downto 16) <= S_AXIS_TDATA(15 downto 0);
-	               when "10" =>
-	                   data_in_reg(47 downto 32) <= S_AXIS_TDATA(15 downto 0);
-	               when "11" =>
-	                   data_in_reg(63 downto 48) <= S_AXIS_TDATA(15 downto 0);
-	               when others => -- 'U', 'X', '-', etc.
-                       data_in_reg <= (others => 'X');
-	           end case;
-	           if dma_last_value = '1' then
-	               data_counter <= b"00";
-	               data_counter_last <= data_counter;
-	               write_enable <= '1';
-	           else
-	               if data_counter = b"11" then
-	                   write_enable <= '1';
-	               else
-	                   write_enable <= '0';
-	               end if;
-	               data_counter <= data_counter+1;
-	           end if;
-	       else
-	           write_enable <= '0';
-	           data_in_reg <= (others => '0');
-	           data_counter <= b"00";
-	       end if;
+	    	dma_enable_delay <= dma_output_enable;
+	    	dma_data_last_delay <= dma_last_value;
+			if (choose_enable = '1') then
+	        	dma_channel_active <= dma_channel;
+	    	end if;
+	       	if (dma_output_enable = '1') then
+	        	choose_enable <= '0';
+	        	-- Choice of the ADC channel 
+	       		case dma_channel_active is
+	            	when "01" => --Channel A
+	                   	case data_counter is
+	                        when "00" =>
+	                            data_in_reg(15 downto 0) <= S_AXIS_TDATA(15 downto 0);
+	                        when "01" =>
+	                           	data_in_reg(31 downto 16) <= S_AXIS_TDATA(15 downto 0);
+	                        when "10" =>
+	                           	data_in_reg(47 downto 32) <= S_AXIS_TDATA(15 downto 0);
+	                        when "11" =>
+	                           	data_in_reg(63 downto 48) <= S_AXIS_TDATA(15 downto 0);
+	                        when others => -- 'U', 'X', '-', etc.
+                                data_in_reg <= (others => 'X');
+	                    end case;
+	               	when "10" => -- Channel B
+	                    case data_counter is
+	                        when "00" =>
+	                        	data_in_reg(15 downto 0) <= S_AXIS_TDATA(31 downto 16);
+	                        when "01" =>
+	                        	data_in_reg(31 downto 16) <= S_AXIS_TDATA(31 downto 16);
+	                        when "10" =>
+	                        	data_in_reg(47 downto 32) <= S_AXIS_TDATA(31 downto 16);
+	                        when "11" =>
+	                            data_in_reg(63 downto 48) <= S_AXIS_TDATA(31 downto 16);
+	                        when others => -- 'U', 'X', '-', etc.
+                                data_in_reg <= (others => 'X');
+	                     end case;
+	               	when "11" => -- Channel A and B
+	                   	case data_counter is 
+	                       	when "00" =>
+	                           	data_in_reg(31 downto 0) <= S_AXIS_TDATA;
+	                       	when "01" =>
+	                           	data_in_reg(63 downto 32) <= S_AXIS_TDATA;
+	                       when others =>
+	                           	data_in_reg <= (others => 'X');
+	                   	end case;
+	               	when others =>
+	                   	data_in_reg <= (others => 'X');
+	           	end case;
+	           
+	           	if dma_last_value = '1' then
+	               	data_counter <= b"00";
+	               	data_counter_last <= data_counter;
+	               	write_enable <= '1';
+	           	else
+					if dma_channel_active = b"11" then
+						if data_counter = b"10" then
+							write_enable <= '1';
+						else
+							write_enable <= '0';
+						end if;							
+					else
+	            		if data_counter = b"11" then
+	                   		write_enable <= '1';
+	            		else
+	                   		write_enable <= '0';
+	            	end if;
+	               data_counter <= data_counter + 1;
+				   end if;
+	           	end if;
+	        else
+	           	write_enable <= '0';
+	           	data_in_reg <= (others => '0');
+	           	data_counter <= b"00";
+	        end if;
 	    end if;
 	  end if;
 	end process;
 	
 	process (AXIS_ACLK)
 	begin
-	  if rising_edge(AXIS_ACLK) then 
-	    if S_AXIS_ARESETN = '0' then
-	      dma_data_valid <= '0';
-	      dma_data <= (others => '0');
-	      dma_data_last <= '0';
-	    else
-	       if write_enable = '1' then
-	           if (dma_data_last_delay = '1') then
-                   for data_index in 0 to 3 loop
-                       if data_index > data_counter_last then
-                           dma_data(data_index*16+15 downto data_index*16) <= (others => '0');
-                       else
-                           dma_data(data_index*16+15 downto data_index*16) <= data_in_reg(data_index*16+15 downto data_index*16);
-                       end if;
-                   end loop;
-               else
-                    dma_data <= data_in_reg;
-               end if;
-               dma_data_last <= dma_data_last_delay;
-	           dma_data_valid <= '1';
-	       else
-	           dma_data <= (others => '0');
-	           dma_data_last <= '0';
-	           dma_data_valid <= '0';
-	       end if;
-	    end if;
-	  end if;
+	  	if rising_edge(AXIS_ACLK) then 
+	    	if S_AXIS_ARESETN = '0' then
+	      		dma_data_valid <= '0';
+	      		dma_data <= (others => '0');
+	      		dma_data_last <= '0';
+	    	else
+	       		if write_enable = '1' then
+	        		if (dma_data_last_delay = '1') then
+						if dma_channel_active /= b"11" then -- Channel A or B data
+                			for data_index in 0 to 3 loop
+                    			if data_index > data_counter_last then
+                        			dma_data(data_index*16+15 downto data_index*16) <= (others => '0');
+                    			else
+                        			dma_data(data_index*16+15 downto data_index*16) <= data_in_reg(data_index*16+15 downto data_index*16);
+                    			end if;
+                			end loop;
+						else -- Both channels
+							for data_index in 0 to 1 loop
+								if data_index > data_counter_last then
+									dma_data(data_index*32+31 downto data_index*32) <= (others => '0');
+								else
+									dma_data(data_index*32+31 downto data_index*32) <= data_in_reg(data_index*32+31 downto data_index*32);
+								end if;
+							end loop;
+						end if;
+               		else
+                    	dma_data <= data_in_reg;
+               		end if;
+               		dma_data_last <= dma_data_last_delay;
+	           		dma_data_valid <= '1';
+	       		else
+	           		dma_data <= (others => '0');
+	           		dma_data_last <= '0';
+	           		dma_data_valid <= '0';
+	       		end if;
+	    	end if;
+		end if;
 	end process;     
 	
 
